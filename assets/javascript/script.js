9b7c468334400dbf0c7675fb4e362859dae424a4
Trevor Harm <trevorharm@users.noreply.github.com>
2018-02-03 10:20:03 -0500

    satWork2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,9 +5,10 @@ $(document).ready(function () {
         authDomain: "myawesometrainscheduler.firebaseapp.com",
         databaseURL: "https://myawesometrainscheduler.firebaseio.com",
         projectId: "myawesometrainscheduler",
-        storageBucket: "",
+        storageBucket: "myawesometrainscheduler.appspot.com",
         messagingSenderId: "588181795224"
-    };
+      };
+      
     firebase.initializeApp(config);
 
     var database = firebase.database();
@@ -39,8 +40,43 @@ $(document).ready(function () {
         // Code for the push to Firebase
         database.ref().push(newTrain);
 
+            // clear input boxes
+            $("#name-input").val("");
+            $("#destination-input").val("");
+            $("#time-input").val("");
+            $("#frequency-input").val("");
     });
+    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 
-
+        console.log(childSnapshot.val());
+      
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().name;
+        var trainDestination = childSnapshot.val().destination;
+        var trainStart = childSnapshot.val().time;
+        var trainFrequency = childSnapshot.val().frquency;
+      
+        // Employee Info
+        console.log(empName);
+        console.log(empRole);
+        console.log(empStart);
+        console.log(empRate);
+      
+        // Prettify the employee start
+        var empStartPretty = moment.unix(empStart).format("MM/DD/YY");
+      
+        // Calculate the months worked using hardcore math
+        // To calculate the months worked
+        var empMonths = moment().diff(moment.unix(empStart, "X"), "months");
+        console.log(empMonths);
+      
+        // Calculate the total billed rate
+        var empBilled = empMonths * empRate;
+        console.log(empBilled);
+      
+        // Add each train's data into the table
+        $("#employee-table > tbody").append("<tr><td>" + empName + "</td><td>" + empRole + "</td><td>" +
+        empStartPretty + "</td><td>" + empMonths + "</td><td>" + empRate + "</td><td>" + empBilled + "</td></tr>");
+    });
 
 };
