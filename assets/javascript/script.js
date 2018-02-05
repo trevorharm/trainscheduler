853a16bbfce22ffc69f515703cd693ea4dd6827f
Trevor Harm <trevorharm@users.noreply.github.com>
2018-02-05 13:35:16 -0500

    MondayWork10

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
         projectId: "myawesometrainscheduler",
         storageBucket: "myawesometrainscheduler.appspot.com",
         messagingSenderId: "588181795224"
-      };
+    };
 
     firebase.initializeApp(config);
 
@@ -19,15 +19,18 @@ $(document).ready(function () {
         // grab user inputs and store in variables
         var name = $("#name-input").val().trim();
         var destination = $("#destination-input").val().trim();
-        var time = moment($("#time-input").val().trim(), "HH:MM").format("hh:mm");
+        var time = moment($("#time-input").val().trim(), "HH:mm").format("");
         var frequency = $("#frequency-input").val().trim();
 
         // validate no empty input
-        // if ($("#name-input").val() == "")
-        // else ($("#destination-input").val() == "")
-        // else if($("#time-input").val() == "")
-        // else if($("#frequency-input").val() == "") {
-        //     alert("Inputs can not be left blank");
+        if (($("#name-input").val() == "")
+        || ($("#destination-input").val() == "")
+                || ($("#time-input").val() == "")
+                || ($("#frequency-input").val() == "")) {
+            alert("Every Input Needs A Value!")
+            return;
+        };
+
 
         // Create variable to hold new train object
         var newTrain = {
@@ -45,43 +48,44 @@ $(document).ready(function () {
         console.log(newTrain.time);
         console.log(newTrain.frequency);
 
-            // clear input boxes
-            $("#name-input").val("");
-            $("#destination-input").val("");
-            $("#time-input").val("");
-            $("#frequency-input").val("");
+        // clear input boxes
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#time-input").val("");
+        $("#frequency-input").val("");
     });
-    database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+    database.ref().on("child_added", function (childSnapshot, prevChildKey) {
 
         console.log(childSnapshot.val());
-      
+
         // Store everything into a variable from Firebase for pushing to HTML
         var trainName = childSnapshot.val().name;
         var trainDestination = childSnapshot.val().destination;
         var trainStart = childSnapshot.val().time;
-        var trainFrequency = childSnapshot.val().frquency;
-      
+        var trainFrequency = childSnapshot.val().frequency;
+
         // New Train Info
         console.log(trainName);
         console.log(trainDestination);
         console.log(trainStart);
         console.log(trainFrequency);
-      
-        // Momentify the train start time
-        // var trainStartPretty = moment.unix(trainStart).format("MM/DD/YY");
-      
+
+
+
         // Calculate the train frequency using hardcore math
         // To calculate the train frequency
         // var nextArrival = moment().diff(moment.unix(trainStart, "X"), "months");
         // console.log(nextArrival);
-      
+
         // Calculate minute away for the next train
         // var minsAway = empMonths * empRate;
         // console.log(minsAway);
-      
+
         // Add each train's data into the table
-        $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
-        trainStartPretty + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
-    });
+        // $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" +
+        // trainStartPretty + "</td><td>" + trainFrequency + "</td><td>" + nextArrival + "</td><td>" + minsAway + "</td></tr>");
+        $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td></tr>");
 
     });
+
+});
