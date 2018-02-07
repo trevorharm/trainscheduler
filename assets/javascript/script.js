fe15b9f386b02ccfdfc16b109b086c79566dd040
Trevor Harm <trevorharm@users.noreply.github.com>
2018-02-07 17:48:21 -0500

    htmlFixTimeoutAdded

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -95,5 +95,9 @@ $(document).ready(function () {
         // $("#train-schedule > tbody").append("<tr><td>" + trainName + "</td><td>" + trainDestination + "</td><td>" + trainFrequency + "</td></tr>");
 
     });
+    // refresh pabe every 1 minute
+    setTimeout(function () { 
+        location.reload();
+      }, 60000);
 
 });
